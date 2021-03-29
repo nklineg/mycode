24eafd91aafaadad503f586bdf8cbee841d4f1b5
nklineg <nklineg@gmail.com>
2021-03-29 19:28:05 +0000

    Revision to Lab 9

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -14,4 +14,4 @@ print("The last item in the list (state): " + my_list[2] )
 
 new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
 
-print("When I ssh into IP address ", new_list[3], " or ", new_list[4], " I am unable to pint ports ", str(new_list[0]), ", ",  new_list[1], ", or ", new_list[2], ".")
+print("When I ", new_list[5], "into IP address ", new_list[3], " or ", new_list[4], " I am unable to pint ports ", str(new_list[0]), ", ",  new_list[1], ", or ", str(new_list[2]), ".")
